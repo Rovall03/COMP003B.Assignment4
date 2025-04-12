18f8bff1b76d58241958795b642865ae4750fbd8
rosa ovalle <you@example.com>
2025-04-11 20:56:40 -0700

    Add project files.

---
+++ COMP003B.Assignment4/Controllers/CatController.cs
@@ -0,0 +1,41 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class CatController : Controller
+    {
+        private static List<Cat> cats = new List<Cat>
+        {
+            new Cat { Id = 1, Name = "Crumpet", Age = 2, Breed = "British Shorthair", Description = "Playful and friendly.", ImageUrl = "https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?b=1&s=170667a&w=0&k=20&c=nOa1R7PGaqOaQscx10FpA5ZNenMeDfs-k6VgmmuY4cc=" },
+            new Cat { Id = 2, Name = "Whiskers", Age = 4, Breed = "Siamese", Description ="Loves naps and cuddles." , ImageUrl = "https://images2.fanpop.com/images/photos/5100000/Cats-wallpaper-cats-5194568-1280-1024.jpg" },
+        };
+
+        public IActionResult Index()
+        {
+            return View(cats);
+        }
+        public IActionResult Details(int id)
+        {
+            // Loop through the list of cats and find the one with the matching ID
+            Cat catFound = null;
+
+            foreach (Cat c in cats)
+            {
+                if
+                    (c.Id == id)
+                {
+                    catFound = c;
+                    break;
+                }
+            }
+            if
+              (catFound == null)
+            {
+                return NotFound();
+            }
+            return View(catFound);
+        }
+
+    }
+}
---
+++ COMP003B.Assignment4/Models/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int Age { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Breed { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        // Instead of uploading, users provide an external image link
+        [Required]
+        [Url]
+        public string ImageUrl { get; set; }
+    }
+}
