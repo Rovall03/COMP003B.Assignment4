f01fe58d821d00a2d1e8bd4f07b317fedd8fb0cf
rosa ovalle <you@example.com>
2025-04-11 21:08:54 -0700

    added adotion controller /view

---
+++ COMP003B.Assignment4/Controllers/AdoptionController.cs
@@ -0,0 +1,34 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+
+    public class AdoptionController : Controller
+    {
+        private static List<AdoptionRequest> adoptionRequests = new List<AdoptionRequest>();
+
+        [HttpGet]
+        public IActionResult Apply(int catId)
+        {
+            var request = new AdoptionRequest { CatId = catId };
+            return View(request);
+        }
+
+        [HttpPost]
+        public IActionResult Apply(AdoptionRequest request)
+        {
+            if (!ModelState.IsValid) return View(request);
+
+            request.Id = adoptionRequests.Count + 1;
+            adoptionRequests.Add(request);
+
+            return RedirectToAction("Success");
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assignment4/Models/AdoptionRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Models
+{
+    
+        public class AdoptionRequest
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string ApplicantName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public string Phone { get; set; }
+
+            [Required]
+            public int CatId { get; set; }
+        }
+    }
+
